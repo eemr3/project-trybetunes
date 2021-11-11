6fa7779729ef5cec93a664f42868014128995a23
Emerson <eemr3@yahoo.com.br>
2021-11-11 15:45:17 -0300

    build(Routes): implementa as rotas"

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,15 +4,21 @@ import Login from '../pages/Login';
 import Search from '../pages/Search';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import NotFound from '../pages/NotFound';
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/profile/edit" component={ ProfileEdit } />
         <Route path="/album/:id" component={ Album } />
         <Route exact path="/" component={ Login } />
         <Route path="/search" component={ Search } />
         <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     );
   }
