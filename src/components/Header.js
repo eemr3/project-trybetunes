6388f553ac552b319d0d82fdc65744e5ad4b1685
Emerson <eemr3@yahoo.com.br>
2021-11-11 19:24:21 -0300

    feat(Header): implementa componente Header - requisito 3

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      isLoading: false,
+    };
+    this.handleGetUser = this.handleGetUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleGetUser();
+  }
+
+  async handleGetUser() {
+    this.setState(({ isLoading: true }));
+    const { name } = await getUser();
+    this.setState(({ user: name, isLoading: false }));
+  }
+
+  render() {
+    const { user, isLoading } = this.state;
+    return (
+      isLoading === true ? (<Loading />)
+
+        : (<header data-testid="header-component">
+          <p>Pesquisa</p>
+          <p>Favoritas</p>
+          <p>Perfil</p>
+          <div data-testid="header-user-name">
+            <span>{user}</span>
+          </div>
+        </header>)
+
+    );
+  }
+}
+
+export default Header;
