58ff675e22e163a6e7306c5fd7cb547fe2bda3f2
Emerson <eemr3@yahoo.com.br>
2021-11-11 21:11:25 -0300

    build(Search): implementa funcionalidade do requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      valueSearch: '',
+      isDisabled: true,
+    };
+    this.handleChangeSearch = this.handleChangeSearch.bind(this);
+  }
+
+  handleChangeSearch({ target }) {
+    const inputLength = 2;
+    const { value } = target;
+    this.setState(({
+      isDisabled: value.length < inputLength,
+      valueSearch: value,
+    }));
+  }
+
   render() {
+    const { isDisabled, valueSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <form>
+          <label htmlFor="search-input">
+            <input
+              id="search-input"
+              type="text"
+              data-testid="search-artist-input"
+              name="valueSearch"
+              value={ valueSearch }
+              onChange={ this.handleChangeSearch }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
