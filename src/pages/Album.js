d18bea1aa8b6957ca8665c7af92b79d68ceadd0e
Emerson <eemr3@yahoo.com.br>
2021-11-12 20:24:23 -0300

    style: corrige problema lint:style

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      musics: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getMusics();
+  }
+
+  getMusics = async (id) => {
+    const response = await getMusics(id);
+    this.setState(({
+      musics: response,
+    }));
+  }
+
   render() {
+    const { musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <h1>Albums</h1>
+        <div>
+          {musics.map((music) => (
+            <div key={ music.trackId }>
+              <h3>{music.artistName}</h3>
+              <ul>
+                <li>
+                  {music.trackName}
+                </li>
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
