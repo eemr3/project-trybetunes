c10edfcb11bccfb926d8f802e55cb1611bf5a51f
Emerson <eemr3@yahoo.com.br>
2021-11-13 20:24:39 -0300

    refactor(Loading): restrutura o componente Loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './Loading.css';
 
 class Loading extends Component {
   render() {
+    const { className } = this.props;
     return (
-      <div className="loading-container">
+      <div className={ className }>
         <h2>Carregando...</h2>
       </div>
     );
   }
 }
+
+Loading.propTypes = {
+  className: PropTypes.string,
+};
+
+Loading.defaultProps = {
+  className: '',
+};
 
 export default Loading;
