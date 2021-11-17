8a5264a42a88f0e6270a908c1629f3d9424e7b20
Emerson <eemr3@yahoo.com.br>
2021-11-16 22:10:16 -0300

    build(Profile): implementa funcionalidade de renderizar informações do perfil requisito - 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataUser: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getDataUser();
+  }
+
+  getDataUser = async () => {
+    const response = await getUser();
+    this.setState({ dataUser: response });
+  }
+
   render() {
+    const { dataUser: { name, image, email, description } } = this.state;
     return (
-      <div data-testid="page-profile">
+      <>
         <Header />
-        <h1>Profile</h1>
-      </div>
+        <div data-testid="page-profile">
+          <div>
+            <img src={ image } alt="alo" data-testid="profile-image" />
+            <h3>{name}</h3>
+            <p>{email}</p>
+            <p>{description}</p>
+          </div>
+        </div>
+        <div>
+          <Link to="/profile/edit">Editar perfil</Link>
+        </div>
+      </>
     );
   }
 }
